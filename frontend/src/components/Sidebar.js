bde448f9af9368724a30c15b94f6e227dba3714e
Nadiia-developer <nadiia.developer777@gmail.com>
2024-06-10 00:41:39 +0300

    Add Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Sidebar = () => {
     return (
         <aside>
-            <h2>Navigation</h2>
+            <h2>Detailed information</h2>
             <nav>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Reports</a></li>
-                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Campaign details</a></li>
+                    <li><a href="#">Offer details</a></li>
+                    <li><a href="#">Clicks details</a></li>
+                    <li><a href="#">Lead details</a></li>
                 </ul>
             </nav>
         </aside>
